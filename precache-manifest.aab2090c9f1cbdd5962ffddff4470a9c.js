12b0c223465ede369ee2716c1e588e7018a3d6db
Omkar <phadtareomi@gmail.com>
2019-03-06 22:10:39 +0530

    Updates

---
+++ precache-manifest.aab2090c9f1cbdd5962ffddff4470a9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b11f067763ca7cd21541",
+    "url": "/TD-React-Challenge-Test-1/static/css/main.928c379d.chunk.css"
+  },
+  {
+    "revision": "b11f067763ca7cd21541",
+    "url": "/TD-React-Challenge-Test-1/static/js/main.b11f0677.chunk.js"
+  },
+  {
+    "revision": "04c3a07e5059ccf77898",
+    "url": "/TD-React-Challenge-Test-1/static/js/runtime~main.04c3a07e.js"
+  },
+  {
+    "revision": "14bf863fa5da7fbc35b2",
+    "url": "/TD-React-Challenge-Test-1/static/js/2.14bf863f.chunk.js"
+  },
+  {
+    "revision": "8535798498a5329d3c754778bfcc075b",
+    "url": "/TD-React-Challenge-Test-1/index.html"
+  }
+];
